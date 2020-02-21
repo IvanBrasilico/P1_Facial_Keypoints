a06cd0f4a77cd80304c4c3429a9588c1f4a4c95c
IvanBrasilico <Iv@n1234>
2020-02-20 22:32:58 -0300

    Versão depois de treinamento e submissão

--- modelsv3.py
+++ modelsv3.py
@@ -2,7 +2,6 @@
 
 import torch
 import torch.cuda
-# from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 # can use the below import should you choose to initialize the weights of your Net
